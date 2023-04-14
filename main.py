c7992de99f94ab510476060a51b0dfe1526a1c97
slave34 <129663699+slave34@users.noreply.github.com>
2023-04-14 18:51:51 +0300

    v1.1

--- main.py
+++ main.py
@@ -9,8 +9,25 @@ window.fill((200,200,200))
 clock = time.Clock()
 mixer.init()
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, w, h):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image),(w, h))
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+        self.speed = player_speed
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update(self):
+        pass
 
 game = True
+finish = False
+platform1 = Player('platform.png', 10,200,5,50,250)
+platform2 = Player('platform2.png', 940,200,5,50,250)
 
 
 while game:
@@ -18,5 +35,9 @@ while game:
         if e.type == QUIT:
             game = False
 
-display.update()
-clock.tick(60)
+    platform1.reset()
+    platform2.reset()
+
+    display.update()
+    clock.tick(60)
+
