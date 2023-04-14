8f200cb7f7b463d1ded6746259af5f4820846056
slave34 <129663699+slave34@users.noreply.github.com>
2023-04-14 18:36:10 +0300

    v1.0
    создал пустое окно с цветом и нажатие на крестик

---
+++ main.py
@@ -0,0 +1,21 @@
+from pygame import *
+from random import randint
+import time as time_module
+
+font.init()
+window = display.set_mode((1000, 700))
+display.set_caption('Пинг-понг')
+window.fill((200,200,200))
+clock = time.Clock()
+mixer.init()
+
+
+game = True
+
+display.update()
+clock.tick(60)
+
+while game:
+    for e in event.get():  # получить все события, происходящие в этот момент
+        if e.type == QUIT:
+            game = False
